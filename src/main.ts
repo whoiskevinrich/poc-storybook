b31ecf97951a253d77b7d54fd75a605c5cd40eab
Kevin Rich <whoiskevinrich@gmail.com>
2022-09-26 13:50:57 -0700

    fixed gitignore & css

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import router from "./router";
 
 import "./assets/main.css";
 import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.use(PiniaVuePlugin);
 Vue.use(ElementUI);
